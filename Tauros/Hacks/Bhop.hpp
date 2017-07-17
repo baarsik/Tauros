25fd62a8a00499e5528092e6a7f9ad1eb489d290
Барс <barsik@cerberus.by>
2017-07-18 01:14:19 +0300

    Небольшой рефакторинг Bhop/AutoStrafe

--- Tauros/Hacks/Bhop.hpp
+++ Tauros/Hacks/Bhop.hpp
@@ -5,24 +5,33 @@ class Bhop
 public:
 	static void CreateMove_Post(C_CSPlayer* pLocal, se::CUserCmd* pCmd)
 	{
-		if (Options::g_bBHopEnabled && pLocal->IsAlive())
+		DoBhop(pLocal, pCmd);
+		DoStrafe(pLocal, pCmd);
+	}
+private:
+	static void DoBhop(C_CSPlayer* pLocal, se::CUserCmd* pCmd)
+	{
+		if (!Options::g_bBHopEnabled || !pLocal->IsAlive())
+			return;
+
+		if (pLocal->GetFlags() & static_cast<int>(se::EntityFlags::FL_ONGROUND))
+			return;
+
+		if (pCmd->buttons & IN_JUMP)
+			pCmd->buttons &= ~IN_JUMP;
+	}
+
+	static void DoStrafe(C_CSPlayer* pLocal, se::CUserCmd* pCmd)
+	{
+		if (!Options::g_bAutoStrafeEnabled)
+			return;
+
+		if (pCmd->mousedx <= 1 && pCmd->mousedx >= -1)
+			return;
+
+		if (pLocal->GetFlags() & static_cast<int>(se::EntityFlags::FL_ONGROUND))
 			return;
 		
-		if (pCmd->buttons & IN_JUMP && !(pLocal->GetFlags() & static_cast<int>(se::EntityFlags::FL_ONGROUND)))
-		{
-			pCmd->buttons &= ~IN_JUMP;
-		}
-
-		if (Options::g_bAutoStrafeEnabled && !(pLocal->GetFlags() & static_cast<int>(se::EntityFlags::FL_ONGROUND)))
-		{
-			if (pCmd->mousedx < 0)
-			{
-				pCmd->sidemove = -450.0f;
-			}
-			else if (pCmd->mousedx > 0)
-			{
-				pCmd->sidemove = 450.0f;
-			}
-		}
+		pCmd->sidemove = pCmd->mousedx < 0 ? -450.f : 450.f;
 	}
 };
